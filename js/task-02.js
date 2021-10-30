dacc7d4c5a38da1b62c761b8474e9d0a20edf9e2
YevhenVK <onegin797@gmail.com>
2021-10-30 19:21:31 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+ 
+const ulElements = document.createElement('li');
+console.log(ulElements)
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+ulElements.innerHTML = list;
+
+// const ulElements = document.querySelector('#ingredients');
+
+// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+// ulElements.innerHTML = list;
