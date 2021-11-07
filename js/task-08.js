4a96f92b0c9b5c0a8f96eb9a2296b1c8d141f72d
YevhenVK <onegin797@gmail.com>
2021-11-07 18:39:17 +0200

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();  //  Страница не перезагружаеться
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    //   return console.log("Alert! Все поля должны быть заполнены!");
+      return alert("Все поля должны быть заполнены");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset(); // Очистка значения полей формы 
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function Switch(obj) {
+            var div = document.getRandomHexColor(obj);
+            if(div.style.backgroundColor === "#6DFC93"){
+                div.style.backgroundColor = "#27D956";
+            }else{
+                div.style.backgroundColor = "#6DFC93";
+            }
+
+        }
