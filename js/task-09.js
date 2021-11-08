f192836311bad4c759ba407e1e8e13c7d1eb27cf
YevhenVK <onegin797@gmail.com>
2021-11-08 17:48:28 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,22 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const button = document.querySelector(".change-color");
-const span = document.querySelector("span");
+const changeColorByClick = document.querySelector(".change-color");
+const spanColorByClick = document.querySelector("span");
 
-button.addEventListener("click", () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
-  console.log("Button was clicked and changed a background-color");
-});
-
-// const span = document.querySelector(".color");
-
-// button.addEventListener("click", () => {
-//   span.textContent = getRandomHexColor();
-// })
-
-
-// запускать рандом функцию,
-//   записывать результат запуска в переменную,
-//     и уже переменную добавлять как цвет на фон и в спан
+changeColorByClick.addEventListener("click", onBackgroundColorChange);
+ 
+function onBackgroundColorChange() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = `${color}`;
+  spanColorByClick.textContent = `${color}`;
+}
