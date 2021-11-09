2ea154404175454656ea188294be3625f40d1ca7
YevhenVK <onegin797@gmail.com>
2021-11-09 09:59:05 +0200

    Task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,10 @@
 const textEl = document.getElementById("text");
-
 const inputEl = document.getElementById("font-size-control");
+inputEl.setAttribute("value", 20);
 
-inputEl.addEventListener("input", (e) => {
+inputEl.addEventListener("input", () => {
   const size = inputEl.value;
   
   textEl.style.fontSize = size + "px";
 });
+
