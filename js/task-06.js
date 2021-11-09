53559d8fd7ccc02b7b641d27ec3a18e48fdcb940
YevhenVK <onegin797@gmail.com>
2021-11-09 08:28:41 +0200

    Task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,23 @@
-const textInput = document.querySelector("input");
+// const textInput = document.querySelector("input");
 
-textInput.addEventListener("blur", () => {
-    if (textInput.value.length >= textInput.dataset.length) {
-    textInput.classList.add('valid');
-    textInput.classList.remove('invalid');
+// textInput.addEventListener("blur", () => {
+//     if (textInput.value.length >= textInput.dataset.length) {
+//     textInput.classList.add('valid');
+//     textInput.classList.remove('invalid');
+//   } else {
+//     textInput.classList.remove('valid');
+//     textInput.classList.add('invalid');
+//   }
+// });
+
+
+document.getElementById("validation-input").onblur = function() {
+  console.log(this.value.length);
+  if (this.getAttribute('data-length') != this.value.length) { 
+    this.classList.add('invalid');
+    this.classList.remove('valid');
   } else {
-    textInput.classList.remove('valid');
-    textInput.classList.add('invalid');
+    this.classList.add('valid');
+    this.classList.remove('invalid');
   }
-});
-
+};
