30dfd3069cbbf205921032e804aa48a8243cf168
YevhenVK <onegin797@gmail.com>
2021-11-09 08:32:19 +0200

    Task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,10 @@
-// const textInput = document.querySelector("input");
-
-// textInput.addEventListener("blur", () => {
-//     if (textInput.value.length >= textInput.dataset.length) {
-//     textInput.classList.add('valid');
-//     textInput.classList.remove('invalid');
-//   } else {
-//     textInput.classList.remove('valid');
-//     textInput.classList.add('invalid');
-//   }
-// });
-
-
-document.getElementById("validation-input").onblur = function() {
-  console.log(this.value.length);
-  if (this.getAttribute('data-length') != this.value.length) { 
-    this.classList.add('invalid');
-    this.classList.remove('valid');
+const textInput = document.querySelector("input");
+textInput.addEventListener("blur", () => {
+    if (textInput.value.length != textInput.dataset.length) {
+    textInput.classList.add('invalid');
+    textInput.classList.remove('valid');
   } else {
-    this.classList.add('valid');
-    this.classList.remove('invalid');
-  }
-};
+    textInput.classList.remove('invalid');
+    textInput.classList.add('valid');
+    }
+});
